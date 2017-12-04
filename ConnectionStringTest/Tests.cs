662fe5b0dc9c405ac6b8c51b1716dcc6e0adff6c
Mikael Frosthage <mikael.frosthage@gmail.com>
2017-12-04 15:42:16 +0100

    Init

---
+++ ConnectionStringTest/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ConnectionStringTest
+{
+    public class Tests
+    {
+        [Fact]
+        public async Task TestConnectionString_ValidConnectionString_NoException()
+        {
+            string connectionString = @"";
+
+            using (var sqlConnection = new SqlConnection(connectionString))
+            {
+                await sqlConnection.OpenAsync();
+            }
+            
+            Assert.True(true);
+        }
+    }
+}
